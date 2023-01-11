e12ebfd0ba5c100473f262d83671cb877097c616
Jackson Price <price.jacksonk@gmail.com>
2023-01-10 22:16:47 -0600

    Add: surveys routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :surveys
+  resources :surveys do
+    member do
+      get 'results'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "surveys#new"
 end
